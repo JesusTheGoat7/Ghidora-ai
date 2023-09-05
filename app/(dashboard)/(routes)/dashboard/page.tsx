c93412e9cfb42d5defa088ce46fba4602f250c22
JesusTheGoat7 <116693360+JesusTheGoat7@users.noreply.github.com>
2023-09-05 09:49:53 -0400

    Initial commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+const DashboardPage = () => {
+  return (
+  <div>
+<p> Dashboard Page(Protected)</p>
+</div>
+  )
+}
+export default DashboardPage;
